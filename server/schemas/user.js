12fbcb23ec84cc9930661bd2b98e39da12dcff6c
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-09-13 17:19:36 +0530

    Update: user schema

--- server/schemas/user.js
+++ server/schemas/user.js
@@ -9,39 +9,9 @@ export default {
             type: 'string'
         },
         {
-            name: 'email',
-            title: 'Email',
+            name: 'image',
+            title: 'Image',
             type: 'string'
-        },
-        {
-            name: 'password',
-            title: 'Password',
-            type: 'string'
-        },
-        {
-            name: 'role',
-            title: 'Role',
-            type: 'string'
-        },
-        {
-            name: 'status',
-            title: 'Status',
-            type: 'string'
-        },
-        {
-            name: 'created_at',
-            title: 'Created At',
-            type: 'datetime'
-        },
-        {
-            name: 'updated_at',
-            title: 'Updated At',
-            type: 'datetime'
-        },
-        {
-            name: 'deleted_at',
-            title: 'Deleted At',
-            type: 'datetime'
         },
     ]
 }
