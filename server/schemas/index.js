4ad831f74630316a4e76de7e9944384b38decf31
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-09-13 19:07:27 +0530

    Add: Export all schemas

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,5 +1,10 @@
 import user from "./user"
+import pin from "./pin"
+import save from "./save"
+import comment from "./comment"
+import postedBy from "./postedBy"
+
 
 export const schemaTypes = [
-    user
+    user, pin, save, comment, postedBy
 ]
