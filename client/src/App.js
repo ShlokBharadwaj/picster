ed67eb01b20444781770e0528da42d4277fc4c55
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-09-14 13:14:15 +0530

    Add: Initial Login and Home pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
 
 const App = () => {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <Routes>
+            <Route path="/*" element={<Home />} />
+            <Route path="login" element={<Login />} />
+        </Routes>
     )
 }
 
