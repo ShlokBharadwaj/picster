e3a7ce229f780ac5b88dd785ac0687055d83a5ca
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-09-14 20:01:14 +0530

    Add: Random background from Unsplash for Login screen

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,9 +3,15 @@ import GoogleLogin from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
 
+import logo from '../assets/picster-logos_white.png';
+
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='flex justify-start items-center flex-col h-screen'>
+      <div className='relative w-full h-full'>
+        <img className='w-full h-full object-cover' src="https://source.unsplash.com/random/?cool,aeathetic,wallpaper" alt="https://source.unsplash.com"/>
+      </div>
+    </div>
   )
 }
 
