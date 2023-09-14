7cbe9ae06e0695449991488dceab48c5b37fb4ae
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-09-14 20:14:08 +0530

    Add: Black overlay over background

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -9,7 +9,9 @@ const Login = () => {
   return (
     <div className='flex justify-start items-center flex-col h-screen'>
       <div className='relative w-full h-full'>
-        <img className='w-full h-full object-cover' src="https://source.unsplash.com/random/?cool,aeathetic,wallpaper" alt="https://source.unsplash.com"/>
+        <img className='w-full h-full object-cover' src="https://source.unsplash.com/random/?cool,aeathetic,wallpaper" alt="https://source.unsplash.com" />
+        <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0' style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+        </div>
       </div>
     </div>
   )
