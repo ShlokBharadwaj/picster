dd58e3c6c7dfe7243d1adb204e783d37fa356474
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-10-09 19:16:15 +0530

    Update: Sanity depricated client to v3

--- client/src/client.js
+++ client/src/client.js
@@ -1,7 +1,7 @@
-import sanityClient from '@sanity/client';
-import { ImageUrlBuilder } from '@sanity/image-url';
+import { createClient } from '@sanity/client';
+import { ImageUrlBuilder } from 'sanity-image-url';
 
-export const client = sanityClient({
+export const client = createClient({
     projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
     apiVersion: '2021-11-16',
