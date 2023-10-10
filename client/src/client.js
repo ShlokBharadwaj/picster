eb2eef9463bbdc7676dfccde252249fb99f6dcf5
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-10-10 13:09:12 +0530

    Update: Sanity api version

--- client/src/client.js
+++ client/src/client.js
@@ -1,10 +1,10 @@
 import { createClient } from '@sanity/client';
-import { ImageUrlBuilder } from 'sanity-image-url';
+import { ImageUrlBuilder } from '@sanity/image-url';
 
 export const client = createClient({
     projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
-    apiVersion: '2021-11-16',
+    apiVersion: '2022-02-01',
     useCdn: true,
     token: process.env.REACT_APP_SANITY_TOKEN,
 });
