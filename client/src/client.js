a595f1b433da6e467e8d01fbfac7e4bbc663e016
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-10-10 13:35:02 +0530

    Update: Use createClient() instead of deprecated sanityClient()

--- client/src/client.js
+++ client/src/client.js
@@ -1,14 +1,14 @@
-import { createClient } from '@sanity/client';
-import { ImageUrlBuilder } from '@sanity/image-url';
+import sanityClient from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
 
-export const client = createClient({
+export const client = sanityClient({
     projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
-    apiVersion: '2022-02-01',
+    apiVersion: '2021-11-16',
     useCdn: true,
     token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
-const builder = ImageUrlBuilder(client);
+const builder = imageUrlBuilder(client);
 
 export const urlFor = (source) => builder.image(source);
