f3fee91235a71f2287af84f2c787041acf1df887
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-02-04 19:58:55 +0530

    Update: Style of Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import { FiPlus, FiUser } from 'react-icons/fi';
+import React, { useState } from 'react';
+import { FiPlus, FiUser, FiChevronDown, FiChevronUp } from 'react-icons/fi';
+import logo from '../assets/picster-logos_white.png';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="flex flex-col bg-gray-800 text-white">
-      <div className="flex justify-between items-center p-5">
-        <div className="font-bold">Logo</div>
-        <input className="flex-grow mx-5 py-2 px-4 rounded-full" type="text" placeholder="Search..." />
+      <div className="flex justify-between items-center p-2">
+        <img
+          src={logo}
+          alt="logo"
+          className="w-16 h-16 md:w-20 md:h-20 lg:w-16 lg:h-16 object-contain max-w-full max-h-full"
+        />
+        <input
+          className="flex-grow mx-5 py-2 px-4 rounded-full"
+          type="text"
+          placeholder="Search..."
+        />
         <div className="flex space-x-5">
           <button className="p-2 rounded-full bg-gray-700 hover:bg-gray-600">
             <FiPlus size={24} />
@@ -16,17 +27,24 @@ const Navbar = () => {
           </button>
         </div>
       </div>
-      <div className="flex space-x-28 p-5">
+      <div className="hidden md:flex space-x-4 p-3">
         <span>Cars</span>
         <span>Nature</span>
         <span>Photography</span>
-        <span>Cars</span>
-        <span>Nature</span>
-        <span>Photography</span>
-        <span>Cars</span>
-        <span>Nature</span>
-        <span>Photography</span>
+        {/* Add more spans for additional navigation items */}
       </div>
+      <div className="md:hidden flex items-center p-5" onClick={() => setIsOpen(!isOpen)}>
+        <span>Menu</span>
+        {isOpen ? <FiChevronUp size={24} /> : <FiChevronDown size={24} />}
+      </div>
+      {isOpen && (
+        <div className="md:hidden flex flex-col p-5">
+          <span>Cars</span>
+          <span>Nature</span>
+          <span>Photography</span>
+          {/* Add more spans for additional navigation items */}
+        </div>
+      )}
     </div>
   )
 }
