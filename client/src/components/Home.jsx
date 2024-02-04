47556a4529a3041ec70693c10da670edfe6bb902
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-02-04 15:54:02 +0530

    Add: Navbar, UserProfile components

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react';
+import { HiMenu } from 'react-icons/hi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { Link, Route, Routes } from 'react-router-dom';
+
+import { Navbar, Login, UserProfile } from './index';
+import { sanityClient } from '../client';
+import logo from '../assets/picster-logos_white.png';
 
 const Home = () => {
   return (
---
+++ client/src/components/index.js
@@ -0,0 +1,3 @@
+export {default as Navbar} from './Navbar';
+export {default as UserProfile} from './UserProfile';
+export {default as Login} from './Login';
