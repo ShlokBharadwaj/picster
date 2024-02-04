6e4526a5c0b16523b85bbb542fa141dd282e0c30
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-02-04 10:49:17 +0530

    Update: Sanity client to v3 and target latest API

--- client/src/client.js
+++ client/src/client.js
@@ -1,14 +1,16 @@
 import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
-export const client = createClient({
+export const sanityClient = createClient({
     projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
-    apiVersion: '2021-11-16',
     useCdn: true,
+    apiVersion: '2023-02-04',
     token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
-const builder = imageUrlBuilder(client);
+const builder = imageUrlBuilder(sanityClient);
 
-export const urlFor = (source) => builder.image(source);
+export const urlFor = (source) => {
+    return builder.image(source);
+};
