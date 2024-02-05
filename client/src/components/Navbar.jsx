460cd9bdc406be6eb723aab386ae08aea684fd15
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-02-05 09:25:00 +0530

    Update: Navbar responsive

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -26,25 +26,25 @@ const Navbar = () => {
 
 
   return (
-    <div className="flex flex-col bg-gray-800 text-white w-screen">
-      <div className="flex justify-between items-center p-1">
+    <div className="flex flex-col bg-gray-800 text-white w-full md:w-screen">
+      <div className="flex justify-between items-center p-2 md:p-4">
         <Link to="/">
           <img
             src={logo}
             alt="logo"
-            className="w-28 h-24 object-cover object-center -m-1"
+            className="w-14 h-12 md:w-28 md:h-24 object-cover object-center -m-1"
           />
         </Link>
         <input
-          className="flex-grow mx-5 py-2 px-4 rounded-full"
+          className="flex-grow mx-0 md:mx-5 py-2 px-4 rounded-full w-1/3 md:w-1/2 bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50"
           type="text"
           placeholder="Search..."
         />
-        <div className="flex space-x-4">
-          <button className="p-1">
+        <div className="flex space-x-0  md:space-x-4">
+          <button className="p-0 md:p-1">
             <FiPlus size={24} className="w-10 h-10 object-cover object-center rounded-full" />
           </button>
-          <button className="p-1">
+          <button className="p-0 md:p-1">
             <Link to={`user-profile/${userInfo?.sub}`}>
               <img
                 src={userInfo?.picture || 'https://via.placeholder.com/150'}
