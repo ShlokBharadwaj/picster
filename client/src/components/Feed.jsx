8900f1cb10be273804dec4f83649e3c1e5cac2be
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-02-06 10:51:14 +0530

    Add: Spinner while pins loads

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { sanityClient } from '../client.js';
+import MasonryLayout from './MasonryLayout.jsx';
+import Spinner from './Spinner.jsx';
 
 const Feed = () => {
+
+  const [loading, setLoading] = useState(false);
+
+  if (!loading) {
+    return <Spinner message="We are adding new ideas to your feed!" />;
+  }
+
   return (
     <div>Feed</div>
   )
