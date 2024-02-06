b1e785564a7e5087c8389734e38e3bc7102050d3
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-02-06 11:05:29 +0530

    Add: Vortex spinner while Loading

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Circles } from 'react-loader-spinner';
+import { Vortex } from 'react-loader-spinner';
 
 const Spinner = ({ message }) => {
   return (
     <div className="flex flex-col justify-center items-center w-screen h-screen">
-      <Circles
-        color="#00BFFF"
-        height={50}
-        width={200}
-        className="m-5"
+      <Vortex
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="vortex-loading"
+        wrapperStyle={{}}
+        wrapperClass="vortex-wrapper"
+        colors={['red', 'green', 'blue', 'yellow', 'orange', 'purple']}
       />
-      <p className="text-2xl mt-4">{message}</p>
+      <p className="text-2xl mt-4 text-center">{message}</p>
     </div>
   )
 }
