08fe693f88889c0b57ec067e554a8c023382b032
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-02-07 20:07:01 +0530

    Add: State hook and navigate hook for Create pin page

--- client/src/components/CreatePin.jsx
+++ client/src/components/CreatePin.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AiOutlineCloudUpload } from 'react-icons/ai';
+import { MdDelete } from 'react-icons/md';
 
-const CreatePin = () => {
+import { sanityClient } from "../client";
+import Spinner from './Spinner';
+import { categories } from "../utils/data";
+
+const CreatePin = ({ user }) => {
+
+  const [title, setTitle] = useState('');
+  const [about, setAbout] = useState('');
+  const [destination, setDestination] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [fields, setFields] = useState(null);
+  const [category, setCategory] = useState(null);
+  const [imageAsset, setImageAsset] = useState(null);
+  const [wrongImageType, setWrongImageType] = useState(false);
+
+  const navigate = useNavigate();
+
   return (
     <div>CreatePin</div>
   )
 }
 
-export default CreatePin
+export default CreatePin;
