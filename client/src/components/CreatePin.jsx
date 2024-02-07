a47f930b277a588eff23afe397d6a2ebfcaa8d0f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-02-08 00:13:08 +0530

    Add: File input upload

--- client/src/components/CreatePin.jsx
+++ client/src/components/CreatePin.jsx
@@ -30,8 +30,56 @@ const CreatePin = ({ user }) => {
       <div className="flex lg:flex-row flex-col justify-center items-center bg-white p-3 lg:p-5 lg:w-4/5 w-full">
         <div className="bg-secondaryColor p-3 flex flex-0.7 w-full">
           <div className="flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420">
-            {!loading &&
-              <Spinner message={"Loading..."}/>}
+            {loading &&
+              <Spinner message={"Loading..."} />}
+            {wrongImageType && (
+              <p className="text-center text-red-500 mb-0 text-xl transition-all duration-100 ease-linear">
+                Please upload a valid image!
+              </p>
+            )}
+            {!imageAsset ? (
+              <label
+                htmlFor="file"
+                className="flex flex-col items-center justify-center w-full h-full cursor-pointer hover:scale-105 transition-all duration-300 ease-linear opacity-60 hover:opacity-100"
+              >
+                <AiOutlineCloudUpload size={60} />
+                <span className="text-xl">Upload Image</span>
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  accept="image/*"
+                  className="hidden"
+                  onChange={(e) => {
+                    const { type, name } = e.target.files[0];
+                    if (type.includes('image')) {
+                      setWrongImageType(false);
+                      setLoading(true);
+
+                      sanityClient.assets
+                        .upload('image', e.target.files[0], { contentType: type, filename: name })
+                        .then((res) => {
+                          setImageAsset(res.url);
+                          // setImageAsset(res);
+                          setLoading(false);
+                        })
+                        .catch((err) => {
+                          console.error("Error uploading image", err);
+                          setLoading(false);
+                        });
+                    } else {
+                      setWrongImageType(true);
+                    }
+                  }}
+                />
+              </label>
+            ) : (
+              <img
+                src={imageAsset}
+                alt="pin"
+                className="w-full h-full object-cover"
+              />
+            )}
           </div>
         </div>
       </div>
