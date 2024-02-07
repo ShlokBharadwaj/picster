bf3a47835abafb00bd93591870daf44b353f4b20
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-02-08 01:24:21 +0530

    Update: Pass users as props to Pins component which pass it to Create Pin component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -36,7 +36,7 @@ const Home = () => {
       <Navbar user={user && user} searchTerm={setSearchTerm} />
       {/* <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-in-out">
         <div className="flex h-screen flex-initial"> */}
-      <Pins searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <Pins searchTerm={searchTerm} setSearchTerm={setSearchTerm} user={userInfo} />
       {/* </div>
       </div> */}
     </>
