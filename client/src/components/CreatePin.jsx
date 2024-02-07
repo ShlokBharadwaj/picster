3a11e663787957d1e42381132bd835884509ae33
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-02-07 20:23:14 +0530

    Add: Container to create post

--- client/src/components/CreatePin.jsx
+++ client/src/components/CreatePin.jsx
@@ -22,11 +22,18 @@ const CreatePin = ({ user }) => {
 
   return (
     <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5">
-      {!fields && (
+      {fields && (
         <p className="text-center text-red-500 mb-0 text-xl transition-all duration-100 ease-linear">
           Please fill all the fields!
         </p>
       )}
+      <div className="flex lg:flex-row flex-col justify-center items-center bg-white p-3 lg:p-5 lg:w-4/5 w-full">
+        <div className="bg-secondaryColor p-3 flex flex-0.7 w-full">
+          <div className="flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420">
+
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
