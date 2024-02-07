6b446b6c9f989ecc240b8fc7af3dd6a8944ee568
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-02-07 14:06:08 +0530

    Update: JSX layout and add user image profile view

--- client/src/components/Pin.jsx
+++ client/src/components/Pin.jsx
@@ -9,12 +9,6 @@ import { fetchUser } from "../utils/fetchUsers";
 
 const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
 
-  console.log('postedBy:', postedBy);
-  console.log('image:', image);
-  console.log('_id:', _id);
-  console.log('destination:', destination);
-  console.log('save:', save);
-
   const [postHovered, setPostHovered] = useState(false);
   const navigate = useNavigate();
 
@@ -52,29 +46,25 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
   };
 
   return (
-    <div className="m-2">
-      <div
-        onMouseEnter={() => setPostHovered(true)}
-        onMouseLeave={() => setPostHovered(false)}
-        onClick={() => navigate(`/pin-detail/${_id}`)}
-        className="relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-300 ease-in-out"
-      >
-        <img src={urlFor(image).width(250).url()} alt="user-post" className="rounded-lg w-full" />
-        {postHovered && (
-          <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
-            <div className="flex flex-col items-center justify-center">
-              <div className="flex items-center justify-center hover:opacity-75 mb-12 text-white cursor-pointer">
-                <Link to={`/pin-detail/${_id}`} className="text-lg font-bold mb-10 text-center">View
-                  <BsFillArrowUpRightCircleFill className="text-2xl ml-2" />
-                </Link>
-              </div>
-              <div className="flex items-center justify-center cursor-pointer">
+    <>
+      <div className="m-2">
+        <div
+          onMouseEnter={() => setPostHovered(true)}
+          onMouseLeave={() => setPostHovered(false)}
+          onClick={() => navigate(`/pin-detail/${_id}`)}
+          className="relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-300 ease-in-out"
+        >
+          <img src={urlFor(image).width(250).url()} alt="user-post" className="rounded-lg w-full" />
+          {postHovered && (
+            <div className="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-[999]">
+              <div className="flex gap-2">
                 <a
                   href={`${image?.asset?.url}?dl=`}
                   download
                   onClick={(e) => e.stopPropagation()}
+                  className="bg-white w-9 h-9 p-2 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
                 >
-                  <MdDownloadForOffline className="text-white text-2xl hover:opacity-75" />
+                  <MdDownloadForOffline />
                 </a>
               </div>
               {alreadySaved ? (
@@ -84,7 +74,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
                     e.stopPropagation();
                     savePin(_id);
                   }}
-                  className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-2 text-base rounded-full hover:shadow-md outline-none mt-10">
+                  className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-2 text-base rounded-full hover:shadow-md outline-none">
                   {save?.length}  Saved
                 </button>
               ) : (
@@ -94,16 +84,43 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
                     e.stopPropagation();
                     savePin(_id);
                   }}
-                  className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-2 text-base rounded-full hover:shadow-md outline-none mt-10">
+                  className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-2 text-base rounded-full hover:shadow-md outline-none">
                   Save
                 </button>
               )}
             </div>
-          </div>
-        )}
+          )}
+        </div>
+        <div className="flex justify-between items-center gap-2 w-full">
+          {destination?.slice(8).length > 0 ? (
+            <a
+              href={destination}
+              target="_blank"
+              rel="noreferrer"
+              className="text-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:opacity-100 hover:shadow-md">
+              <BsFillArrowUpRightCircleFill />
+              {destination?.slice(8,17)}...
+            </a>
+          ) : undefined}  
+          {postedBy?._id === userInfoString.sub && (
+            <button
+              type="button"
+              onClick={(e) => {
+                e.stopPropagation();
+                deletePin(_id);
+              }}
+              className="bg-white p-2 rounded-full w-9 h-9 flex items-center justify-center text-black opacity-75 hover:opacity-100 outline-none">
+              <AiTwotoneDelete />
+            </button>
+          )}
+        </div>
       </div>
-    </div>
-  )
-}
+      <Link to={`/user-profile/${postedBy?._id}`} className="flex gap-2 mt-2 items-center">
+        <img src={postedBy?.picture} alt="user-profile" className="w-10 h-10 rounded-full object-cover" />
+        <h1 className="text-lg font-bold">{postedBy?.name}</h1>
+      </Link>
+    </>
+  );
+};
 
 export default Pin;
