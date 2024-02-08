97410de9d5df623f038b7c0d4365b3ded2ef382a
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-02-08 18:24:19 +0530

    Update: Copy layout from Pin Details section

--- client/src/components/PinDetails.jsx
+++ client/src/components/PinDetails.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import { MdDownloadForOffline } from 'react-icons/md';
+import { AiOutlineCloudUpload } from 'react-icons/ai';
+import { MdDelete } from 'react-icons/md';
 
 
 import { sanityClient, urlFor } from '../client.js';
@@ -51,25 +53,96 @@ const PinDetails = ({ user }) => {
   };
 
   return (
-    <div className="flex flex-col xl:flex-row m-auto bg-white max-w-[1500px] border-r-[32px] ">
-      <div className="flex justify-center items-center md:items-start flex-initial">
-        <img
-          src={pinDetails?.image && urlFor(pinDetails.image).url()}
-          alt={"user-pin"}
-          className="w-1/3 h-1/3 object-cover rounded-3xl"
-        />
-      </div>
-      <div className="w-full p-5 flex-1 xl:min-w-620">
-        <div className="flex items-center justify-between">
-          <div className="flex gap-2 items-center">
-            <a
-              href={`${pinDetails.image?.asset?.url}?dl=`}
-              download
-              onClick={(e) => e.stopPropagation()}
-              className="bg-white w-9 h-9 p-2 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none ml-1"
+    <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5 transition-all duration-200 animate-fade-in">
+
+      <p className="text-center text-red-500 mb-0 text-xl transition-all duration-100 ease-linear">
+        Please fill all the fields!
+      </p>
+
+      <div className="flex lg:flex-row flex-col justify-center items-center bg-white p-3 lg:p-5 lg:w-4/5 w-full">
+        <div className="bg-secondaryColor p-3 flex flex-0.7 w-full">
+          <div className="flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420">
+
+            <p className="text-center text-red-500 mb-0 text-xl transition-all duration-100 ease-linear">
+              Please upload a valid image!
+
+            </p>
+
+            <label
+              htmlFor="file"
+              className="flex flex-col items-center justify-center w-full h-full cursor-pointer hover:scale-105 transition-all duration-300 ease-linear opacity-60 hover:opacity-100"
             >
-              <MdDownloadForOffline />
-            </a>
+              <AiOutlineCloudUpload size={60} />
+              <span className="text-xl">Upload Image</span>
+              <input
+                type="file"
+                id="file"
+                name="file"
+                accept="image/*"
+                className="hidden"
+              />
+            </label>
+
+            <div className="w-full h-full relative">
+              <img
+                alt="pin"
+                className="w-full h-full object-contain"
+              />
+              <button
+                type="button"
+                className="bg-white p-2 rounded-full w-9 h-9 flex items-center justify-center text-black opacity-75 hover:opacity-100 outline-none absolute top-0 right-0 transition-all duration-500 hover:animate-zoom-in">
+                <MdDelete size={20} />
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-1 flex-col gap-6 lg:pl-5 mt-5 w-full">
+
+          <div className="flex gap-2 my-2 items-center bg-white  rounded-md w-full">
+            <img
+              src={"https://via.placeholder.com/150"}
+              alt="user-profile"
+              className="w-10 h-10 rounded-full"
+            />
+            <p className="text-xl font-bold">Shlok Bharadwaj</p>
+          </div>
+
+          <input
+            type="text"
+            placeholder="Title"
+            value={"title"}
+            className="border-2 border-gray-300 p-2 rounded-md w-full font-bold outline-none"
+          />
+          <textarea
+            placeholder="About"
+            value={"about"}
+            className="border-2 border-gray-300 p-2 rounded-md w-full resize-none outline-none"
+          />
+          <input
+            type="text"
+            placeholder="Destination"
+            value={"destination"}
+            className="border-2 border-gray-300 p-2 rounded-md w-full font-bold outline-none"
+          />
+          <div className="flex flex-col">
+            <div>
+              <p className="mb-2 font-semibold">Choose pin Category:</p>
+              <select
+                value={"category"}
+                className="border-2 border-gray-300 p-2 rounded-md w-full capitalize outline-none"
+              >
+                <option value={"other"} className="bg-white">Select Category</option>
+                
+              </select>
+            </div>
+            <div>
+              <button
+                type="button"
+                className="text-black font-bold p-3 rounded-md w-full hover:shadow-lg transition-all duration-300 ease-linear mt-4 shadow-2xl outline-1">
+                Create Pin
+              </button>
+            </div>
           </div>
         </div>
       </div>
