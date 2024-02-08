0580afabc590f772b497a2dbdae7b83c8e15ac20
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-02-08 16:54:23 +0530

    Update: Intial Pin details UI

--- client/src/components/PinDetails.jsx
+++ client/src/components/PinDetails.jsx
@@ -51,7 +51,29 @@ const PinDetails = ({ user }) => {
   };
 
   return (
-    <div>PinDetails</div>
+    <div className="flex flex-col xl:flex-row m-auto bg-white max-w-[1500px] border-r-[32px] ">
+      <div className="flex justify-center items-center md:items-start flex-initial">
+        <img
+          src={pinDetails?.image && urlFor(pinDetails.image).url()}
+          alt={"user-pin"}
+          className="w-1/3 h-1/3 object-cover rounded-3xl"
+        />
+      </div>
+      <div className="w-full p-5 flex-1 xl:min-w-620">
+        <div className="flex items-center justify-between">
+          <div className="flex gap-2 items-center">
+            <a
+              href={`${pinDetails.image?.asset?.url}?dl=`}
+              download
+              onClick={(e) => e.stopPropagation()}
+              className="bg-white w-9 h-9 p-2 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none ml-1"
+            >
+              <MdDownloadForOffline />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
