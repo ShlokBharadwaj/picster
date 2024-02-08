72f34eaeff88fab1511d13ffa9d1e9b3935e5895
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-02-08 13:13:59 +0530

    Add: Queries and imports for Pin details

--- client/src/components/PinDetails.jsx
+++ client/src/components/PinDetails.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+import { MdDownloadForOffline } from 'react-icons/md';
 
-const PinDetails = () => {
+
+import { sanityClient, urlFor } from '../client.js';
+import MasonryLayout from './MasonryLayout.jsx';
+import { pinDetailMorePinQuery, pinDetailQuery } from '../utils/data.js';
+import Spinner from './Spinner.jsx';
+
+const PinDetails = ({ user }) => {
   return (
     <div>PinDetails</div>
   )
 }
 
-export default PinDetails
+export default PinDetails;
