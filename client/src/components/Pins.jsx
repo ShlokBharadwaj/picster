a4e4b62381d6f47d5eba9a258fae5d7a977f0ca3
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-02-10 00:24:47 +0530

    Update: Whole Route padding to accumulate the User Profile's bg image

--- client/src/components/Pins.jsx
+++ client/src/components/Pins.jsx
@@ -8,18 +8,15 @@ const Pins = (user) => {
   const [searchTerm, setSearchTerm] = useState('');
 
   return (
-    <div className="px-2 md:px-5">
-      <div className="h-full">
-        <Routes>
-          <Route path="/" element={<Feed />} />
-          <Route path="/category/:categoryId" element={<Feed />} />
-          <Route path="/pin-detail/:pinId" element={<PinDetails user={user} />} />
-          <Route path="/create-pin" element={<CreatePin user={user} ></CreatePin>} />
-          <Route path="/search" element={< Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />} />
-          <Route path="/user-profile/:userID" element={<UserProfile />} />
-
-        </Routes>
-      </div>
+    <div className="h-full">
+      <Routes>
+        <Route path="/" element={<div className="px-2 md:px-5"><Feed /></div>} />
+        <Route path="/category/:categoryId" element={<div className="px-2 md:px-5"><Feed /></div>} />
+        <Route path="/pin-detail/:pinId" element={<div className="px-2 md:px-5"><PinDetails user={user} /></div>} />
+        <Route path="/create-pin" element={<div className="px-2 md:px-5"><CreatePin user={user} ></CreatePin></div>} />
+        <Route path="/search" element={<div className="px-2 md:px-5"><Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} /></div>} />
+        <Route path="/user-profile/:userID" element={<UserProfile />} />
+      </Routes>
     </div>
   )
 }
--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -8,9 +8,47 @@ import { sanityClient } from "../client";
 import MasonryLayout from "./MasonryLayout";
 import Spinner from "./Spinner";
 
+const randomImgSrc = "https://source.unsplash.com/random/1600x900";
+
 const UserProfile = () => {
+
+  const [user, setUser] = useState(null);
+  const [createdPins, setCreatedPins] = useState(null);
+  const [text, setText] = useState('Created Pins');
+  const [activeBtn, setActiveBtn] = useState('createdPins');
+  const navigate = useNavigate();
+  const { userID } = useParams();
+
+  useEffect(() => {
+    const fetchUserQuery = userQuery(userID);
+
+    sanityClient.fetch(fetchUserQuery)
+      .then((user) => setUser(user[0]))
+      .catch(console.error);
+  }, [userID]);
+
+  if (!user) {
+    return <Spinner message={"Loading User Profile"} />
+  }
+
+  console.log(user);
+
   return (
-    <div>UserProfile</div>
+    <div className="relative pb-2 h-full justify-center items-center">
+      <div className="flex flex-col pb-5">
+        <div className="relative flex flex-col mb-7">
+          <div className="flex flex-col justify-center items-center">
+            <img src={randomImgSrc}
+              alt={"cover-photo"}
+              className="w-screen h-64 shadow-md object-cover"
+            />
+            <img src={user.image}
+              alt="profile-photo"
+              className="rounded-full w-20 h-20 -mt-10 shadow-xl object-cover z-50" />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
