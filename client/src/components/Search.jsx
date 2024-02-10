8d291d6450f579426dae5e8f517253ad0cbda4c3
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-02-10 13:26:02 +0530

    Add: Access to search term

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import MasonryLayout from './MasonryLayout';
+import { sanityClient } from "../client";
+import { feedQuery, searchQuery } from "../utils/data";
+import Spinner from './Spinner';
 
-const Search = () => {
+const Search = ({ searchTerm }) => {
+
+    console.log("Search Term: ", searchTerm);
+
     return (
         <div>Search</div>
     )
 }
 
-export default Search
+export default Search;
